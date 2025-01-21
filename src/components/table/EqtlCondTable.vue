f93ae15db45b54c01c5eafb863aaf20f774500b5
Colin Gross <grosscol@gmail.com>
2025-01-21 11:59:51 -0500

    revert to sending credentials in request.

--- src/components/table/EqtlCondTable.vue
+++ src/components/table/EqtlCondTable.vue
@@ -127,6 +127,7 @@ export default {
         headers: {
           "Content-type": "application/json; charset=utf-8",
         },
+        credentials: "include"
       },
       ajaxContentType: "json",
       ajaxRequesting: () => {
