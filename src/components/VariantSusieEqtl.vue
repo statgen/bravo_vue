4e85d3ce4ddc0606357a0ee97b51d3b4a989701e
Colin Gross <grosscol@gmail.com>
2025-01-25 12:15:55 -0500

    WIP stub eqtl variant info fields

--- src/components/VariantSusieEqtl.vue
+++ src/components/VariantSusieEqtl.vue
@@ -7,8 +7,9 @@
         </div>
       </div>
       <div class="row">
-        <div class="col-5 col-sm-7 text-left text-truncate">Susie Measure</div>
-        <div class="col-7 col-sm-5 text-right">Value</div>
+        <span class="col-sm-4 text-left text-truncate">Tissue</span>
+        <span class="col-sm-4 text-left text-truncate">Phenotype</span>
+        <span class="col-sm-4 text-right">PIP</span>
       </div>
     </div>
   </div>
