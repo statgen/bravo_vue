e4abd685f76f317f9fec4761b8151593b360d71f
Colin Gross <grosscol@gmail.com>
2025-02-20 13:32:06 -0500

    Fix height of gene bar position highlight line

--- src/components/SegBars.vue
+++ src/components/SegBars.vue
@@ -21,7 +21,7 @@
       <g id="labelSection" class="tx__label"></g>
     </g>
     <line id="SegHighlightLine" class="tx__hi-line"
-      x1="0" y1="0" x2="0" y2="200" visibility=hidden></line>
+      x1="0" y1="0%" x2="0" y2="100%" visibility=hidden></line>
   </svg>
 </div>
 </template>
